7ce07c266e921396ca7e14e16b839fc3361d96db
Won Rhim <wwrk22@gmail.com>
2023-03-04 23:12:52 -0500

    Add #generate_and_set_secret_code with unit test

--- game.rb
+++ game.rb
@@ -1,3 +1,5 @@
+require_relative './board.rb'
+
 class Game
   
   def initialize(board, player_1, player_2, num_rounds=1)
@@ -7,4 +9,12 @@ class Game
     @num_rounds = num_rounds
   end
 
+  ##
+  # Create an array of four randomly generated integers within [0, 5] to
+  # represent a row of code pegs and set it in the board as the secret
+  # code.
+  def generate_and_set_secret_code
+    secret_code = [rand(6), rand(6), rand(6), rand(6)]
+    @board.secret_code = secret_code
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,7 +5,13 @@ RSpec.describe Game do
 
   describe "#generate_and_set_secret_code" do
 
+    let(:board) { instance_double(Board) }
+    subject(:game) { described_class.new(board, nil, nil) }
 
+    it "sets a valid secret code in the game board" do 
+      expect(board).to receive(:secret_code=)
+      game.generate_and_set_secret_code
+    end
 
   end
 
